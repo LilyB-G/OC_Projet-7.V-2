7afffa5641e8f0508e45a99c23554a6f52986d8e
Lily <justpunkrock@hotmail.fr>
2022-11-19 18:59:59 +0100

    First steps

---
+++ frontend/src/main.js
@@ -0,0 +1,13 @@
+import Vue from 'vue'
+import App from './App.vue'
+import '@mdi/font/css/materialdesignicons.css' // Ensure you are using css-loader
+import vuetify from './plugins/vuetify'
+import router from './router'
+
+Vue.config.productionTip = false
+
+new Vue({
+  vuetify,
+  router,
+  render: h => h(App)
+}).$mount('#app')
